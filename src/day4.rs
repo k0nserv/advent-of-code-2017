b6f7af9a8240bffae56e89c935851fc75886db18
Hugo Tunius <hugo.tunius@gmail.com>
2017-12-04 18:44:47 +0000

    [Day 4] Solve day 4 start two

--- src/day4.rs
+++ src/day4.rs
@@ -1,19 +1,75 @@
+use std::iter::FromIterator;
 use std::collections::HashSet;
 
-pub fn solve(input: &str) -> u32 {
-    input.trim().split("\n").map(|phrase| phrase.split_whitespace()).map(|words| {
-        let vec = words.collect::<Vec<&str>>();
-        let len = &vec.len();
+trait Validator {
+    fn is_valid(&self, phrase: &Vec<&str>) -> bool;
+}
+
+struct UniquenessValidator {}
+
+impl Validator for UniquenessValidator {
+    fn is_valid(&self, phrase: &Vec<&str>) -> bool {
+        let len = &phrase.len();
         let mut set: HashSet<&str> = HashSet::with_capacity(*len);
-        set.extend(vec);
+        set.extend(phrase);
 
         set.len() == *len
-    }).filter(|&valid| valid).collect::<Vec<bool>>().len() as u32
+    }
+}
+
+struct AnagramValidator {}
+
+impl Validator for AnagramValidator {
+    fn is_valid(&self, phrase: &Vec<&str>) -> bool {
+        let sorted = phrase.iter().map(|&word| {
+                                           let mut chars = word.chars().collect::<Vec<char>>();
+                                           chars.sort_by(|a, b| b.cmp(a));
+
+                                           String::from_iter(chars)
+                                       });
+
+        let len = sorted.len();
+
+        let mut set: HashSet<String> = HashSet::with_capacity(len);
+        set.extend(sorted);
+
+        set.len() == len
+    }
+}
+
+pub fn parse(input: &str) -> Vec<Vec<&str>> {
+    input.trim()
+        .split("\n")
+        .map(|phrase| phrase.split_whitespace())
+        .map(|words| words.collect::<Vec<&str>>())
+        .collect()
+}
+
+pub fn solve_star_one(input: &str) -> u32 {
+    let phrases = parse(input);
+    let validator = UniquenessValidator {};
+
+    phrases.iter()
+        .map(|phrase| validator.is_valid(phrase))
+        .filter(|&valid| valid)
+        .collect::<Vec<bool>>()
+        .len() as u32
+}
+
+pub fn solve_star_two(input: &str) -> u32 {
+    let phrases = parse(input);
+    let validator = AnagramValidator {};
+
+    phrases.iter()
+        .map(|phrase| validator.is_valid(phrase))
+        .filter(|&valid| valid)
+        .collect::<Vec<bool>>()
+        .len() as u32
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{solve};
+    use super::{solve_star_one, solve_star_two};
 
     #[test]
     fn test_cases_star_one() {
@@ -22,6 +78,18 @@ mod tests {
             aa bb cc dd aa
             aa bb cc dd aaa
             ";
-        assert_eq!(solve(input), 2);
+        assert_eq!(solve_star_one(input), 2);
+    }
+
+    #[test]
+    fn test_cases_star_two() {
+        let input = "
+            abcde fghij
+            abcde xyz ecdab
+            a ab abc abd abf abj
+            iiii oiii ooii oooi oooo
+            oiii ioii iioi iiio
+            ";
+        assert_eq!(solve_star_two(input), 3);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -54,12 +54,13 @@ mod tests {
         use std::fs::File;
         use std::io::Read;
 
-        use day4::{solve};
+        use day4::{solve_star_one, solve_star_two};
 
         let mut input = String::new();
         let mut f = File::open("day4.txt").expect("Unable to open file");
         f.read_to_string(&mut input).expect("Unable to read string");
 
-        assert_eq!(solve(&input), 451);
+        assert_eq!(solve_star_one(&input), 451);
+        assert_eq!(solve_star_two(&input), 223);
     }
 }
