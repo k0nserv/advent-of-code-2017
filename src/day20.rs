660575fed616dfa89c2e0ca6653b84018831e90d
Hugo Tunius <h@tunius.se>
2018-11-30 16:47:32 +0000

    Delete day20 tests

--- src/day20.rs
+++ src/day20.rs
@@ -172,18 +172,3 @@ pub fn star_two(input: &str) -> usize {
 
     particles.iter().filter(|x| x.is_some()).count()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::{star_one, star_two};
-
-    #[test]
-    fn test_star_one() {
-        assert_eq!(star_one(""), 1)
-    }
-
-    #[test]
-    fn test_star_two() {
-        assert_eq!(star_two(""), 1)
-    }
-}
